0042f8dfe9f534574265fa5f829335d18f9f593f
Hallie Sala <halliesala19@gmail.com>
2023-10-04 11:56:40 -0400

    Flask boilerplate

--- server/app.py
+++ server/app.py
@@ -1,17 +1,24 @@
 from flask import Flask, jsonify, request, session
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
-
-
+from flask_restful import Api, Resource 
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
 from models import db
 
+# ----- ENVIRONMENT VARIABLES ----- #
+load_dotenv()
+FLASK_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
+
+# ----- FLASK SETUP ----- #
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
 
-# Set default db to app.db
+# Ensure integrity of session data
+app.secret_key = FLASK_SECRET_KEY
+
+# Set default db to app.db & create binds for other dbs
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
-
-# 
 app.config['SQLALCHEMY_BINDS'] = {
     'heartysoupsinternational': 'sqlite:///heartysoupsinternational.db',
     'planterson': 'sqlite:///planterson.db',
@@ -20,14 +27,25 @@ app.config['SQLALCHEMY_BINDS'] = {
     'housebrand2': 'sqlite:///housebrand2.db',
 }
 
+# 'False' to avoid overhead of tracking all CRUD updates
+# I may want to change this later to keep detailed logs
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Keep JSON responses human-readable
 app.json.compact = False
 
+# Safely encrypt passwords
 bcrypt = Bcrypt(app)
 
+# Handle database migrations
 migrate = Migrate(app, db)
-
 db.init_app(app)
 
+# So the browser doesn't complain about CORS
+CORS(app)
+
+# ----- API SETUP ----- #
+
+# We may eventually want a v2 -- so, this is v1!
 URL_PREFIX = '/api/v1'
 
