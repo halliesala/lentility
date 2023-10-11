51795aadc7b7168fced0641a9648b71dfc9aed8c
Hallie Sala <halliesala19@gmail.com>
2023-10-11 11:11:25 -0400

    improve pagination

--- client/src/components/ShopPage.jsx
+++ client/src/components/ShopPage.jsx
@@ -8,32 +8,52 @@ import SearchTag from './SearchTag';
 export default function ShopPage() {
     const { canonicalProducts } = useLoaderData()
     const { user, setUser } = useOutletContext()
-    const [itemsPerPage, setItemsPerPage] = useState(16)
 
+    // Pagination
+    const [itemsPerPage, setItemsPerPage] = useState(16)
+    const [activePage, setActivePage] = useState(1)  
+    
     // All active search bar queries
     const [searchTerms, setSearchTerms] = useState([])
 
     // Current search bar query
     const [searchBarContent, setSearchBarContent] = useState('')
-
-    // Pagination
-    const [activePage, setActivePage] = useState(1)  
-    const totalPages = Math.ceil(canonicalProducts.length / itemsPerPage)
-
-    console.log("Shop Page", canonicalProducts)
     
+    // Pagination    
     function removeTag(tag) {
         console.log("TODO: Remove tag")
         setSearchTerms(searchTerms.filter(t => t !== tag))
     }
+    
+    const displayItems = canonicalProducts
+    .filter(cp => {
+        // Return true if cp (name, manufacturer, sku, or suppliers) matches ALL search terms
+        // strip non-alphanumeric characters and make lowercase
+        const corpus = ('' 
+        + cp.name 
+        + cp.manufacturer.name 
+        + cp.manufacturer_sku)
+        .toLowerCase().replace(/[^a-z0-9]/g, '')
+        return searchTerms.every(st => corpus.includes(st)) && corpus.includes(searchBarContent.toLowerCase().replace(/[^a-z0-9]/g, ''))
+    })
+    .map(cp => {
+        return (
+            <Grid.Column key={cp.id}>
+                    <ProductCard user={user} cp={cp} />
+                </Grid.Column>
+            )
+        })
 
+    console.log("displayItems", displayItems)
+    
+    // Pagination numbers -- ex., "Showing 1-16 of 32 items"
+    const firstItemNum = (activePage-1) * itemsPerPage + 1;
+    const lastItemNum = Math.min(displayItems.length, (activePage * itemsPerPage));
+    const totalItems = displayItems.length;
+    const totalPages = Math.ceil(totalItems / itemsPerPage)
+        
     return (
         <>
-            <PaginationExamplePagination 
-                activePage={activePage} 
-                setActivePage={setActivePage} 
-                totalPages={totalPages}
-            />
             <SearchBar 
                 searchBarContent={searchBarContent} 
                 setSearchBarContent={setSearchBarContent} 
@@ -49,32 +69,23 @@ export default function ShopPage() {
             
             <Grid columns={4}>
                 {
-                    canonicalProducts
-                    .filter(cp => {
-                        // Return true if cp (name, manufacturer, sku, or suppliers) matches ALL search terms
-                        // strip non-alphanumeric characters and make lowercase
-                        const corpus = ('' 
-                            + cp.name 
-                            + cp.manufacturer.name 
-                            + cp.manufacturer_sku)
-                            .toLowerCase().replace(/[^a-z0-9]/g, '')
-                        return searchTerms.every(st => corpus.includes(st)) && corpus.includes(searchBarContent.toLowerCase().replace(/[^a-z0-9]/g, ''))
-                    })
-                    .map(cp => {
-                        return (
-                            <Grid.Column key={cp.id}>
-                                <ProductCard user={user} cp={cp} />
-                            </Grid.Column>
-                        )
-                    })
+                    displayItems
                     .slice((activePage - 1) * itemsPerPage, activePage * itemsPerPage)
                 }
             </Grid>
+            <div className="pagination-div">
+                <PaginationBar 
+                    activePage={activePage} 
+                    setActivePage={setActivePage} 
+                    totalPages={totalPages}
+                />
+                <p>Showing {firstItemNum}-{lastItemNum} of {totalItems} items</p>
+            </div>
         </>
     )
 }
 
-function PaginationExamplePagination({ activePage, setActivePage, totalPages}) {
+function PaginationBar({ activePage, setActivePage, totalPages}) {
 
     function handlePageChange(e) {
         setActivePage(e.target.getAttribute('value'))
