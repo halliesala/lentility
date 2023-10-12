088c43fe7080a1a82ed192a6c58378937d601334
Hallie Sala <halliesala19@gmail.com>
2023-10-12 16:16:19 -0400

    price details can be hidden

--- client/src/components/SupplierProductsTable.jsx
+++ client/src/components/SupplierProductsTable.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
 import { useOutletContext } from "react-router-dom"
+import { Icon, Table } from "semantic-ui-react"
 
 export default function SupplierProductsTable({order_item, prices}) {
     const { user, setUser } = useOutletContext()
     const [pricesLastRefreshed, setPricesLastRefreshed] = useState(new Date())
+    const [hideTable, setHideTable] = useState(true)
 
     const [priceInfo, setPriceInfo] = useState(prices)
 
@@ -24,31 +26,41 @@ export default function SupplierProductsTable({order_item, prices}) {
 
     return (
         <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Supplier</th>
-                        <th>Price</th>
-                        <th>Free Shipping Threshold</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        order_item.canonical_product.suppliers
-                        .map(s => {
-                            return (
-                                <tr key={s.id}>
-                                    <td>{s.name}</td>
-                                    <td>{(priceInfo?.[s.name]?.price)?.toFixed(2) ?? 'Connect Vendor'}</td>
-                                    <td>{priceInfo?.[s.name]?.free_shipping_threshold ?? 'Connect Vendor'}</td>    
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
-            <button onClick={refreshPrice}>Refresh Prices</button>
-            <p>prices last refreshed: {pricesLastRefreshed.toLocaleString()}</p>
+            <button className='show-detail-button' onClick={() => setHideTable(!hideTable)}>{hideTable ? "Show Details" : "Hide Details"}</button>
+            {/* <Icon name={hideTable ? 'angle double down' : 'angle double up'} className='show-detail-button' onClick={() => setHideTable(!hideTable)} hidden={hideTable} /> */}
+            <div className='detail-div' hidden={hideTable}>
+                <Table celled >
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Supplier</Table.HeaderCell>
+                            <Table.HeaderCell>Price</Table.HeaderCell>
+                            <Table.HeaderCell>Free Shipping Threshold</Table.HeaderCell>
+                            <Table.HeaderCell>Shipping Cost</Table.HeaderCell>
+                            <Table.HeaderCell>Days to Ship</Table.HeaderCell>
+                            <Table.HeaderCell>Stock</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {
+                            order_item.canonical_product.suppliers
+                            .map(s => {
+                                return (
+                                    <Table.Row key={s.id}>
+                                        <Table.Cell>{s.name}</Table.Cell>
+                                        <Table.Cell>{(priceInfo?.[s.name]?.price)?.toFixed(2) ?? 'Connect Vendor'}</Table.Cell>
+                                        <Table.Cell>{priceInfo?.[s.name]?.free_shipping_threshold ?? 'Connect Vendor'}</Table.Cell>
+                                        <Table.Cell>{priceInfo?.[s.name]?.shipping_cost ?? 'Connect Vendor'}</Table.Cell>   
+                                        <Table.Cell>{priceInfo?.[s.name]?.days_to_ship ?? 'Connect Vendor'}</Table.Cell> 
+                                        <Table.Cell>{priceInfo?.[s.name]?.stock ?? 'Connect Vendor'}</Table.Cell>
+                                    </Table.Row>
+                                )
+                            })
+                        }
+                    </Table.Body>
+                </Table>
+                <button onClick={refreshPrice}>Refresh Prices</button>
+                <p>prices last refreshed: {pricesLastRefreshed.toLocaleString()}</p>
+            </div>
         </>
     )
 }
